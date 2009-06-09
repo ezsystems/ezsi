dbfa6f0c28b3092f1702f44bc830a276e7b33779
Jérôme Renard <jr@ez.no>
2009-06-09 13:44:30 +0000

    - Added PHPUnit tests
    git-svn-id: http://svn.projects.ez.no/ezsi/trunk@20 0cc08a9f-4d52-4612-8d77-e0d2f54f1ed6

---
+++ tests/suite.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File containing the eZSITestSuite class
+ *
+ * @copyright Copyright (C) 1999-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @package tests
+ */
+
+class eZSITestSuite extends ezpTestSuite
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setName( "eZSI Test Suite" );
+
+        $this->addTestSuite( 'eZSIBlockFunctionTest' );
+    }
+
+    public static function suite()
+    {
+        return new self();
+    }
+}
+
+?>
