7835c336d85516ad76f8902da5319ddf3fa29f9c
Jérôme Renard <jr@ez.no>
2009-06-09 14:34:13 +0000

    - Updated version number
    git-svn-id: http://svn.projects.ez.no/ezsi/trunk@27 0cc08a9f-4d52-4612-8d77-e0d2f54f1ed6

--- ezinfo.php
+++ ezinfo.php
@@ -27,7 +27,7 @@ class eZSIInfo
     static function info()
     {
         return array( 'name' => "eZ SI",
-                      'version' => "1.1",
+                      'version' => "1.1-dev",
                       'copyright' => "Copyright (C) 1999-2009 eZ systems AS",
                       'license' => "GNU General Public License v2.0"
                     );
